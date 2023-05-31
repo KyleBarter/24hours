fc5cc3d8f7a85dd0b8f257a79b5d49192ef23170
KyleBarter <kylebarter4@gmail.com>
2023-05-31 16:10:42 +0100

    pseudo added

---
+++ controllers/activities.js
@@ -0,0 +1,17 @@
+const Activity = require('../models/activity');
+const User = require('../models/user');
+
+//? index function
+
+
+//? show function
+
+
+//? new activity
+
+
+//? update activity
+
+
+//? delete activity
+
