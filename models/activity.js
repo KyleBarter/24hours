fd0a46ebe80d57f107cb8b9cd8b520418130027d
KyleBarter <kylebarter4@gmail.com>
2023-06-01 12:56:46 +0100

    oauth almost implemented

--- models/activity.js
+++ models/activity.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
+//?activity schema
 const activitySchema = new Schema ({
     activity: String,
     activityType: {
         type: String,
         enum: ['Cooking', 'Exercise', 'Gardening', 'Hobbies', 'Reading', 'Religious practice', 'Self care', 'Sleep', 'Socialising', 'Studying', 'Wellbeing', 'Work']
     },
+    //checkboxes to select multiple
     day: {
         type: String,
         enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     },
+    //time will need to increment from 30mins, 1hour, 1hour 30mins, 2hours etc
+    // possibly use enum  and have it increment? Then in EJS just print the hours so it's not 
+    // a user picking 600 minutes trying to figure out how many hours
     time: {
-        type: Number
+        type: Number,
+        min: 30,
+        max: 1400
+    },
+    recordedGoal: {
+        type: Boolean
     }
 })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '24Hours' });
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+  }
+));
 
 module.exports = router;
